4086f46a30bc1d3996c2febc672d76a289d0488a
Douglas Lima <douglasproglima@gmail.com>
2017-03-09 23:33:33 -0300

    11 - Ocultar/Mostrar Componentes

--- public/js/angular/config.js
+++ public/js/angular/config.js
@@ -2,11 +2,11 @@ var $=jQuery;
 var myApp = angular.module('myApp',['ngRoute']);
 
 var produtos=[
-    {descricao: 'Mouse', preco: '15.50', data: '1488580813117'},
-    {descricao: 'Leitor de DVD', preco: '15.50', data: '1488880813117'},
-    {descricao: 'Gabinente Gamer', preco: '365.32', data: '1488980813117'},
-    {descricao: 'Teclado', preco: '98.59', data: '1489080813117'},
-    {descricao: 'Monitor', preco: '1500.55', data: '1489280813117'},
+    {descricao: 'mouse', preco: '15.50', data: '1488580813117'},
+    {descricao: 'leitor de DVD', preco: '15.50', data: '1488880813117'},
+    {descricao: 'gabinente Gamer', preco: '365.32', data: '1488980813117'},
+    {descricao: 'teclado', preco: '98.59', data: '1489080813117'},
+    {descricao: 'monitor', preco: '1500.55', data: '1489280813117'},
 ];
 
  myApp.config(function($routeProvider){
@@ -16,7 +16,7 @@ var produtos=[
  });
 
  myApp.filter('capitalize', function () {
-    return function (input, scope) {
+    return function (input) {
         input = input.toLowerCase();
         return input.substr(0,1).toUpperCase()+input.substr(1);
     }
