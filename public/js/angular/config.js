180fc4ca57b1c2e3a5201e2f453763277c40b407
Douglas Lima <douglasproglima@gmail.com>
2017-03-09 22:08:52 -0300

    7 - Rota Home Atualizada

--- public/js/angular/config.js
+++ public/js/angular/config.js
@@ -1,6 +1,5 @@
 var $=jQuery;
-
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['ngRoute']);
 
 var produtos=[
     {descricao: 'Mouse', preco: '15.50', data: '1488580813117'},
@@ -10,20 +9,7 @@ var produtos=[
     {descricao: 'Monitor', preco: '1500.55', data: '1489280813117'},
 ];
 
-/*
- var $=jQuery;
- var produtos=[
- {descricao: 'Mouse', preco: '15.50', data: '1488580813117'},
- {descricao: 'Leitor de DVD', preco: '15.50', data: '1488880813117'},
- {descricao: 'Gabinente Gamer', preco: '365.32', data: '1488980813117'},
- {descricao: 'Teclado', preco: '98.59', data: '1489080813117'},
- {descricao: 'Monitor', preco: '1500.55', data: '1489280813117'},
- ];
-
- var myApp = angular.module('myApp',['ngRoute']);
-
  myApp.config(function($routeProvider){
- $routeProvider.when('/',{templateUrl: '/app/views/home.html', controller: 'homeController'}),
- $routeProvider.when('/contato',{templateUrl: '/app/views/contato.html', controller: 'contatoController'})
+    $routeProvider
+        .when('/',{templateUrl: 'app/views/home.html', controller: 'homeController'});
  });
- * */
--- public/js/angular/controllers/controllers.js
+++ public/js/angular/controllers/controllers.js
@@ -1,22 +1,9 @@
-myApp.controller('produtosController', function($scope) {
+/*myApp.controller('produtosController', function($scope) {
     $scope.produtos = produtos;
-});
+});*/
 
 
-/*
+
  myApp.controller('homeController', function($scope) {
-
- var produtos=[
- {descricao: 'Mouse', preco: '15.50', data: '1488580813117'},
- {descricao: 'Leitor de DVD', preco: '15.50', data: '1488880813117'},
- {descricao: 'Gabinente Gamer', preco: '365.32', data: '1488980813117'},
- {descricao: 'Teclado', preco: '98.59', data: '1489080813117'},
- {descricao: 'Monitor', preco: '1500.55', data: '1489280813117'},
- ];
- $scope.produtos = produtos;
+    $scope.produtos = produtos;
  });
-
- myApp.controller('contatoController', function($scope) {
-
- });
-* */
