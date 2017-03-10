19ce73b5adf4f58e6448e7208373c55c20cbc9e8
Douglas Lima <douglasproglima@gmail.com>
2017-03-09 23:06:41 -0300

    9 - Método Deletar

--- public/js/angular/controllers/controllers.js
+++ public/js/angular/controllers/controllers.js
@@ -1,5 +1,11 @@
 myApp.controller('homeController', function($scope) {
     $scope.produtos = produtos;
+
+    //Método para deletar os produtos
+    $scope.deletar = function(index) {
+        var indexOf = $scope.produtos.indexOf(index);
+        $scope.produtos.splice(indexOf, 1);
+    }
 });
 
 myApp.controller('contatoController', function($scope) {
