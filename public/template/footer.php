463f9c972172438cd6fe78444404ca00265cef18
Douglas Lima <douglasproglima@gmail.com>
2017-03-11 22:19:06 -0300

    12 - Menu e Estilização da Pág.

---
+++ public/template/footer.php
@@ -0,0 +1,11 @@
+
+        <script src="public/js/angular/angular.js"></script>
+        <script src="public/js/jquery/jquery.js"></script>
+        <script src="public/tether/js/tether.js"></script>
+        <script src="public/bootstrap/js/bootstrap.js"></script>
+        <script src="public/js/angular/route.js"></script>
+        <script src="public/js/angular/config.js"></script>
+        <script src="public/js/angular/controllers/controllers.js"></script>
+
+    </body>
+</html>
---
+++ public/template/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br" ng-app="myApp">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>AngularJS</title>
+
+        <link rel="stylesheet" href="public/bootstrap-3.3.7/dist/css/bootstrap.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
+        <link rel="stylesheet" href="public/css/styles.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
+
+<!--        <link href="public/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+<!--        <link href="public/css/font-awesome.win.css" rel="stylesheet">-->
+<!--        <link href="public/css/estilo.css" rel="stylesheet">-->
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid" style="margin-left: 60px; ">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Agenda de Contatos</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Início</a></li>
+                    <li><a href="/">Produtos</a></li>
+                    <li><a href="#">Perfil</a></li>
+                    <li><a href="#">Ajuda</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
