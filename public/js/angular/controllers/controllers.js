0c98e04737e8b6c909c5ae6320a6382b7ae32a26
Douglas Lima <douglasproglima@gmail.com>
2017-03-11 23:16:06 -0300

    14 - Método Cadastrar (Teste)

--- public/js/angular/controllers/controllers.js
+++ public/js/angular/controllers/controllers.js
@@ -4,7 +4,15 @@ myApp.controller('homeController', function($scope) {
     //Método para deletar os produtos
     $scope.deletar = function(index) {
         var indexOf = $scope.produtos.indexOf(index);
-        $scope.produtos.splice(indexOf, 1);
+        produtos.splice(indexOf, 1);
+    }
+
+    //Método para salvar os produtos
+    $scope.salvar = function () {
+        //unshift: Método responsavel por add um elemento no início do array()
+        $scope.produto.data = '1489284813117';
+        produtos.unshift($scope.produto);
+        console.log($scope.produto);
     }
 
     /*include no controller
