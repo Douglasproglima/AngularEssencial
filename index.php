a55ab7a938b8e2fd9976cf8ec5cc1f45940fa9e9
Douglas Lima <douglasproglima@gmail.com>
2017-03-11 22:18:39 -0300

    12 - Menu e Estilização da Pág.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'public/template/header.php'; ?>
+
+        <div id="main" class="container-fluid" style="margin-top: 20px;">
+            <h2>Projeto Angular</h2>
+            <div ng-view></div>
+        </div>
+
+<?php include 'public/template/footer.php'; ?>
